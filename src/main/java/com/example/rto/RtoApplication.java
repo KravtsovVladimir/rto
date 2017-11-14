9ad9c1372fdf80a05a758ae5701f15fee3c5c738
Volodymyr Kravtsov <vladimirkravtsov13@gmail.com>
2017-11-14 17:13:11 +0200

    "first commit"

---
+++ src/main/java/com/example/rto/RtoApplication.java
@@ -0,0 +1,12 @@
+package com.example.rto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtoApplication.class, args);
+	}
+}
